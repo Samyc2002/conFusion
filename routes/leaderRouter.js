31c954429f2106092ca8f9fb521c7b6d539307f3
Samy <200020040@iitdh.ac.in>
2021-05-23 13:45:59 +0530

    Authentication Half Done:

---
+++ routes/leaderRouter.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Leaders = require('../models/leaders');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyparser.json());
+
+// collection router goes here
+
+leaderRouter.route('/')
+.get((req, res, next) => {
+    Leaders.find({})
+    .then((leaders) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leaders);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+})
+.post((req, res, next) => {
+    Leaders.create(req.body)
+    .then((leader) => {
+        console.log('Leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported');
+})
+.delete((req, res, next) => {
+    Leaders.remove({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+// specific router goes here
+
+leaderRouter.route('/:leaderId')
+.get((req, res, next) => {
+    Leaders.findById(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+})
+.post((req, res, next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /promotions/' + req.params.leaderId);
+})
+.put((req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId, { $set: req.body }, { new: true})
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+})
+.delete((req, res, next) => {
+    Leaders.findByIdAndDelete(req.params.leaderId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(resp);
+    }, (err) => {
+        next(err);
+    })
+    .catch((err) => {
+        next(err);
+    });
+});
+
+module.exports = leaderRouter;
